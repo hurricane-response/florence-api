9c9c1ed60aefb44a47c7839aca9d7d8777dcd0a3
Jesse Wolgamott <jesse@comal.io>
2017-08-31 16:22:51 -0500

    Allow CORS for all the front end

--- Gemfile
+++ Gemfile
@@ -16,6 +16,7 @@ gem 'jbuilder', '~> 2.5'
 gem 'sucker_punch'
 gem 'google-api-client', '~> 0.11'
 gem 'dotenv-rails'
+gem 'rack-cors', :require => 'rack/cors'
 
 group :development, :test do
   gem 'capybara', '~> 2.13'
--- config/application.rb
+++ config/application.rb
@@ -15,6 +15,13 @@ module HarveyApi
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
 
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins '*'
+        resource '*', :headers => :any, :methods => [:get, :post, :options]
+      end
+    end
+
     config.active_job.queue_adapter = :sucker_punch
   end
 end
