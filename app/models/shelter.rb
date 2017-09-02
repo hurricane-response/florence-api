3ed2cbe6b47db847a19094c13ef0de8c676d37bf
Jesse Wolgamott <jesse@comal.io>
2017-09-02 15:22:58 -0500

    Allow entry of geocoding

--- app/models/shelter.rb
+++ app/models/shelter.rb
@@ -1,9 +1,9 @@
 class Shelter < ApplicationRecord
-  ColumnNames = ["accepting", "address", "address_name", "city", "county", "id", "latitude", "longitude", "notes", "pets", "phone", "shelter", "source", "supply_needs", "updated_at", "updated_by", "volunteer_needs"]
+  ColumnNames = ["accepting", "address", "address_name", "city", "county", "id", "latitude", "longitude", "notes", "pets", "phone", "shelter", "source", "supply_needs", "updated_by", "volunteer_needs", "last_updated", "latitude", "longitude"]
 
   HeaderNames = ColumnNames.map(&:titleize)
 
-  UpdateFields = ["accepting", "address", "address_name", "city", "county", "notes", "pets", "phone", "shelter", "source", "supply_needs", "updated_by", "volunteer_needs"]
+  UpdateFields = ["accepting", "address", "address_name", "city", "county", "notes", "pets", "phone", "shelter", "source", "supply_needs", "updated_by", "volunteer_needs", "last_updated", "latitude", "longitude"]
 
   has_many :drafts, as: :record
 
