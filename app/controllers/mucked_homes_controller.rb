a48f653e88f5c99d72848adb7f97ff414c64ea3f
Alan Hong <xiupan@gmail.com>
2017-09-02 17:47:56 -0500

    wut

--- app/controllers/mucked_homes_controller.rb
+++ app/controllers/mucked_homes_controller.rb
@@ -1,5 +1,26 @@
 class MuckedHomesController < ApplicationController
+
   def index
     @muckedHomes = MuckedHome.all
   end
+
+  def new
+    @muckedHome = MuckedHome.new
+  end
+
+  def create
+    if(user_signed_in? && current_user.admin?)
+      @muckedHome = MuckedHome.new(mucked_home_update_params)
+      if @muckedHome.save
+        redirect_to @muckedHome, notice: 'muckedHome was successfully created.'
+      else
+        render :new
+      end
+    end
+  end
+
+  def mucked_home_update_params
+    params.require(:muckedHome).permit(MuckedHome::UpdateFields).keep_if { |_,v| v.present? }
+  end
+
 end
