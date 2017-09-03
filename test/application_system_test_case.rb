7be59657bf13e61183c876b88c543d57cd33ba3f
Jesse Wolgamott <jesse@comal.io>
2017-09-03 14:28:17 -0500

    selenium_chrome_headless Didnt work :(... will try the 8 second delay

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -1,5 +1,6 @@
 require "test_helper"
 
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
-  driven_by :selenium_chrome_headless#, using: :chrome, screen_size: [1400, 1400]
+  #driven_by :selenium_chrome_headless
+  driven_by :selenium, using: :chrome, screen_size: [1400, 1400]
 end
