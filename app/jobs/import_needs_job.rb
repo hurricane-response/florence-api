f536772322c6a4233569257503de7e0d76e6198b
Ryan B. Harvey <ryan.b.harvey@ted.com>
2017-09-02 19:35:53 -0500

    Update data import jobs to use API Importer instead of Google Sheets.

--- app/jobs/import_needs_job.rb
+++ app/jobs/import_needs_job.rb
@@ -1,36 +1,18 @@
-require 'google/apis/sheets_v4'
 class ImportNeedsJob < ApplicationJob
   queue_as :default
 
   def perform(*args)
-    puts "Starting ImportNeedsJob #{Time.now}"
-
-    service = Google::Apis::SheetsV4::SheetsService.new
-    service.client_options.application_name = 'Harvey Needs'
-    scopes =  ['https://www.googleapis.com/auth/drive']
-    service.authorization = Google::Auth.get_application_default(scopes)
-    spreadsheet_id = '14GHRHQ_7cqVrj0B7HCTVE5EbfpNFMbSI9Gi8azQyn-k'
-    range = 'Needs!A1:N'
-    response = service.get_spreadsheet_values(spreadsheet_id, range)
-
-    headers = response.values.shift.map(&:parameterize).map(&:underscore)
-
-    ApplicationRecord.connection.transaction do
-      Need.delete_all
-      response.values.each do |row|
-        values = Hash[headers.zip(row)]
-
-        values["are_volunteers_needed"] = values["are_volunteers_needed"] =~ /no/i ? false : true
-
-        values["are_supplies_needed"] = values["are_supplies_needed"] =~ /no/i ? false : true
-
-        Need.create! values
-      end
-      puts "ImportNeedsJob Complete - {#{response.values.count - 1}}"
-
+    puts "Starting ImportNeedsJob #{Time.now} using API"
+    needs = APIImporter.needs
+    needs.each do |need|
+      # needs and cleanPhone are derived fields
+      # updatedAt is set on save to the database
+      Need.create! need.except("needs", "cleanPhone", "updatedAt")
     end
+    puts "ImportNeedsJob Complete - {#{needs.count}}"
 
     # schedule an update, but it's throttled to 1 every 10 minutes
     ScheduleAmazonFetchJob.perform_later
   end
+
 end
