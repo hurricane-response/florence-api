c5fb709d7a763902aa0d2b51fe601e378b0e46e0
Alex Close <ajclose@gmail.com>
2017-09-05 12:01:35 -0500

    Fix test

--- test/controllers/splash_controller_test.rb
+++ test/controllers/splash_controller_test.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 
 class SplashControllerTest < ActionDispatch::IntegrationTest
   test "should get index" do
-    get splash_index_url
+    get root_path
     assert_response :success
   end
 
