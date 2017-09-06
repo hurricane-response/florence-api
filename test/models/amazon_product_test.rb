3b04c5689f3517d72c58a7ea0a659b64b9a39763
Jesse Wolgamott <jesse@comal.io>
2017-09-06 11:22:04 -0500

    Store pricing from amazon, display on api

--- test/models/amazon_product_test.rb
+++ test/models/amazon_product_test.rb
@@ -4,8 +4,8 @@ class AmazonProductTest < ActiveSupport::TestCase
 
   test ":active does not include disabled" do
     AmazonProduct.destroy_all
-    formula = AmazonProduct.create! need: "baby formula", asin: "XXXXXX", detail_url: "http", category_general: "baby", category_specific: "baby food"
-    AmazonProduct.create! disabled: true, need: "opens at 6", asin: "XXXXXX", detail_url: "http"
+    formula = AmazonProduct.create! price_in_cents: 2000, need: "baby formula", asin: "XXXXXX", detail_url: "http", category_general: "baby", category_specific: "baby food"
+    AmazonProduct.create! price_in_cents: 100, disabled: true, need: "opens at 6", asin: "XXXXXX", detail_url: "http"
     assert_equal [formula], AmazonProduct.active
   end
 
@@ -13,5 +13,12 @@ class AmazonProductTest < ActiveSupport::TestCase
     AmazonProduct.destroy_all
     formula = AmazonProduct.create! need: "baby formula", asin: "XXXXXX", detail_url: "http"
     assert_equal [], AmazonProduct.active
+  end
+
+  test ":active does not include missing pricing" do
+    AmazonProduct.destroy_all
+    formula = AmazonProduct.create! price_in_cents: 2000, need: "baby formula", asin: "XXXXXX", detail_url: "http", category_general: "baby", category_specific: "baby food"
+    AmazonProduct.create! price_in_cents: 0, disabled: true, need: "opens at 6", asin: "XXXXXX", detail_url: "http"
+    assert_equal [formula], AmazonProduct.active
   end
 end
