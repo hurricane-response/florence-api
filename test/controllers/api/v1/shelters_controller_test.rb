505e5046aaa5de4ed2accb0a4e52124055838e13
Ryan B. Harvey <ryan.b.harvey@ted.com>
2018-09-14 22:16:29 -0500

    Tests are still wonky, but the feature works, with relevant tests passing and manual testing successful.

--- test/controllers/api/v1/shelters_controller_test.rb
+++ test/controllers/api/v1/shelters_controller_test.rb
@@ -16,7 +16,7 @@ class Api::SheltersControllerTest < ActionDispatch::IntegrationTest
     json = JSON.parse(response.body)
     assert_equal count, json["shelters"].length
     assert_equal count, json["meta"]["result_count"]
-    assert_equal :true, json["meta"]["filters"]["accepting"]
+    assert_equal "true", json["meta"]["filters"]["accepting"]
   end
 
   test "shelters are not returned after they are archived" do
