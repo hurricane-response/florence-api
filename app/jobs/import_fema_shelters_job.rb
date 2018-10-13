7eda38000c6d895c2f9ed70921ce6beeed5ab3cb
Eric L. Truitte, MBA <omnilord@privateerconsulting.com>
2018-10-13 13:55:35 -0400

    Adding a deduplication step that compares deltas on Latitude and Longitude, and reorders the tests by precision, starting with exact address, then moving to coordinates, and finally using the source field
    Closes #59

--- app/jobs/import_fema_shelters_job.rb
+++ app/jobs/import_fema_shelters_job.rb
@@ -22,14 +22,25 @@ private
     # an unindexed scan of the database against several columns of text
     #
     # TODO: Use Arel for the where named functions
+    #
+    # arel = Shelter.arel_table
 
-    arel = Shelter.arel_table
-    cnt = Shelter.unscope(:where).where(
+    cnt = Shelter.unscope(:where).where('LOWER(TRIM(address)) = ?', data[:address].strip.downcase).count
+    unless cnt.positive?
+      lat = data[:latitude].to_f
+      lon = data[:longitude].to_f
+      delta = 0.0002
+      cnt += Shelter.unscope(:where).where(
+             '(latitude between ? and ?) AND (longitude between ? and ?)',
+             lat - delta, lat + delta, lon - delta, lon + delta
+          ).count
+    end
+    cnt += Shelter.unscope(:where).where(
           'LOWER(TRIM(shelter)) = ? AND LOWER(TRIM(city)) = ? AND LOWER(TRIM(state)) = ? AND LOWER(TRIM(zip)) = ?',
           data[:shelter].strip.downcase, data[:city].strip.downcase, data[:state].strip.downcase, data[:zip].strip.downcase
-        ).count
-    cnt += Shelter.unscope(:where).where('LOWER(TRIM(address)) = ?', data[:address].strip.downcase).count
-    cnt += Shelter.unscope(:where).where('LOWER(TRIM(source)) = ?', data[:source].strip.downcase).count
+        ).count unless cnt.positive?
+    cnt += Shelter.unscope(:where).where('LOWER(TRIM(source)) = ?', data[:source].strip.downcase).count unless cnt.positive?
+
 
     if cnt > 0
       logger.info "Duplicate: #{data[:shelter]} @ #{data[:address]}"
